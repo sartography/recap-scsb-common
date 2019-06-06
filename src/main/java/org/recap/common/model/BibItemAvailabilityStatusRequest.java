ddff8b9dff2f89297f6b912845cab8fe4bf3d8fa
Aaron Louie <ajlouie@ux-ninja.com>
2019-06-06 16:39:55 -0400

    Adds Builder

--- src/main/java/org/recap/common/model/BibItemAvailabilityStatusRequest.java
+++ src/main/java/org/recap/common/model/BibItemAvailabilityStatusRequest.java
@@ -1,8 +1,10 @@
 package org.recap.common.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class BibItemAvailabilityStatusRequest {
     private String bibliographicId;
     private String institutionId;
