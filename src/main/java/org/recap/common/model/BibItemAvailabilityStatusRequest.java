0a41a0c2860fb15782540cf0067415e1ab7b2165
Aaron Louie <ajlouie@ux-ninja.com>
2019-06-06 16:10:39 -0400

    Adds BibItemAvailabilityStatusRequest

---
+++ src/main/java/org/recap/common/model/BibItemAvailabilityStatusRequest.java
@@ -0,0 +1,47 @@
+package org.recap.common.model;
+
+/**
+ * Created by akulak on 3/3/17.
+ */
+public class BibItemAvailabilityStatusRequest {
+
+    private String bibliographicId;
+
+    private String institutionId;
+
+    /**
+     * Gets bibliographic id.
+     *
+     * @return the bibliographic id
+     */
+    public String getBibliographicId() {
+        return bibliographicId;
+    }
+
+    /**
+     * Sets bibliographic id.
+     *
+     * @param bibliographicId the bibliographic id
+     */
+    public void setBibliographicId(String bibliographicId) {
+        this.bibliographicId = bibliographicId;
+    }
+
+    /**
+     * Gets institution id.
+     *
+     * @return the institution id
+     */
+    public String getInstitutionId() {
+        return institutionId;
+    }
+
+    /**
+     * Sets institution id.
+     *
+     * @param institutionId the institution id
+     */
+    public void setInstitutionId(String institutionId) {
+        this.institutionId = institutionId;
+    }
+}
